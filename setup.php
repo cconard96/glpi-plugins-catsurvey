1e292ae0e88007ddba2ef7514af11d6e8ab42023
Philippe GODOT <philippe.godot@probesys.com>
2021-04-15 15:09:19 +0200

    upgrade version number and check 9.5.5 compatibility

--- setup.php
+++ setup.php
@@ -1,34 +1,48 @@
 <?php
 // Version of the plugin
-define('PLUGIN_CATSURVEY_VERSION', "9.4.2");
+define('PLUGIN_CATSURVEY_VERSION', "9.5.0");
 // Minimal GLPI version, inclusive
-define ("PLUGIN_CATSURVEY_GLPI_MIN_VERSION", "9.4");
+define("PLUGIN_CATSURVEY_GLPI_MIN_VERSION", "9.4");
 // Maximum GLPI version, exclusive
-define ("PLUGIN_CATSURVEY_GLPI_MAX_VERSION", "9.5");
+define("PLUGIN_CATSURVEY_GLPI_MAX_VERSION", "9.6");
 
-function plugin_version_catsurvey() {
-
-    return array('name'           => "Catsurvey",
-                 'version'        => PLUGIN_CATSURVEY_VERSION,
-                 'author'         => 'Maxime Bonillo, Philippe GODOT',
-                 'license'        => 'GPLv2+',
-                 'homepage'       => 'http://www.probesys.com',
-                 'minGlpiVersion' => PLUGIN_CATSURVEY_GLPI_MIN_VERSION);// For compatibility / no install in version < 0.80
-
+function plugin_version_catsurvey()
+{
+    return [
+        'name' => 'Catsurvey',
+        'version' => PLUGIN_CATSURVEY_VERSION,
+        'author' => '<a href="http://www.probesys.com">Probesys</a>',
+        'license' => 'GLPv3',
+        'homepage'       => 'http://www.probesys.com',
+        'requirements'   => [
+         'glpi'   => [
+            'min' => PLUGIN_CATSURVEY_GLPI_MIN_VERSION,
+            'max' => PLUGIN_CATSURVEY_GLPI_MAX_VERSION,
+         ],
+         'php'    => [
+            'min' => '7.0'
+         ]
+        ]
+    ];
 }
 
-function plugin_catsurvey_check_prerequisites() {
+function plugin_catsurvey_check_prerequisites()
+{
+    try {
+        if (version_compare(GLPI_VERSION, PLUGIN_CATSURVEY_GLPI_MIN_VERSION, '<')) {
+            throw new Exception('This plugin requires GLPI >= ' . PLUGIN_CATSURVEY_GLPI_MIN_VERSION);
+        }
 
-    $success = true;
-    if (version_compare(GLPI_VERSION, PLUGIN_CATSURVEY_GLPI_MIN_VERSION, 'lt')) {
-       echo 'This plugin requires GLPI >= ' . PLUGIN_CATSURVEY_GLPI_MIN_VERSION . '<br>';
-       $success = false;
+        $prerequisites_check_ok = true;
+    } catch (Exception $e) {
+        echo $e->getMessage();
     }
-    return $success;
+
+    return $prerequisites_check_ok;
 }
 
-function plugin_catsurvey_check_config($verbose=false) {
-
+function plugin_catsurvey_check_config($verbose=false)
+{
     if (true) { // No configuration check
         return true;
     }
@@ -39,13 +53,12 @@ function plugin_catsurvey_check_config($verbose=false) {
     return false;
 }
 
-function plugin_init_catsurvey() {
+function plugin_init_catsurvey()
+{
     global $PLUGIN_HOOKS;
 
     $PLUGIN_HOOKS['csrf_compliant']['catsurvey'] = true;
 
     Plugin::registerClass('PluginCatsurveyCatsurvey');
     Plugin::registerClass('PluginCatsurveyCatsurvey', array('addtabon' => array('ITILCategory')));
-
 }
-
